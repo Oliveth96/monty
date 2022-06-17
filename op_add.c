d628b927a1609780ecf638bb0fac0676a582cf1d
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-17 02:01:36 +0100

    adds the top two elements of the stack.

--- op_add.c
+++ op_add.c
@@ -1,19 +1,25 @@
 #include "monty.h"
 
 /**
- * op_add - add the top two elements on the stack
- * @sp: stack pointer
+ * add - Adds the top two elements of the stack
+ * @stack: The head of the stack
+ * @line_number: The line on which the error occurred
+ *
+ * Return: Nothing
  */
-void op_add(stack_t **sp)
+void add(stack_t **stack, unsigned int line_number)
 {
-	int to_add = 0;
+	stack_t *temp = *stack;
+	unsigned int a = 0, b = 0, length = 0;
 
-	if (!(*sp && *sp != (*sp)->next))
-		pfailure("L%u: can't add, stack too short\n", op_env.lineno);
+	length = count_stack(*stack);
 
-	to_add = (*sp)->n;
+	if (length < 2)
+		handle_error(ERR_ADD_USG, NULL, line_number, NULL);
 
-	op_pop(sp);
-
-	(*sp)->n += to_add;
+	a = temp->n;
+	b = temp->next->n;
+	temp->next->n = a + b;
+	*stack = temp->next;
+	free(temp);
 }
