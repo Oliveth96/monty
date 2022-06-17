a3d6567e3884a323659f1dd7e98bc516a4b62f77
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-17 02:05:08 +0100

    prints the value at the top of the stack, followed by a new line.

--- op_pint.c
+++ op_pint.c
@@ -1,13 +1,17 @@
 #include "monty.h"
 
 /**
- * op_pint - print the value at the top of the stack
- * @sp: stack pointer
+ * pint - Prints the value at the top of the stack,
+ * followed by a new line.
+ * @stack: The head of the stack
+ * @line_number: The line on which the error occurred
+ *
+ * Return: Nothing
  */
-void op_pint(stack_t **sp)
+void pint(stack_t **stack, unsigned int line_number)
 {
-	if (*sp)
-		printf("%d\n", (*sp)->n);
-	else
-		pfailure("L%u: can't pint, stack empty\n", op_env.lineno);
+	if (*stack == NULL)
+		handle_error(ERR_PINT_USG, NULL, line_number, NULL);
+
+	printf("%d\n", (*stack)->n);
 }
