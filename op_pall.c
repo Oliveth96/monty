51788fb26d566a5e7ddd2935124b037f9afdfa08
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-17 02:11:05 +0100

    opcode push pushes an element to the stack, prints all the values on the stack, starting from the top of the stack.

--- op_pall.c
+++ op_pall.c
@@ -1,20 +1,25 @@
 #include "monty.h"
 
 /**
- * op_pall - print each element on the stack
- * @sp: stack pointer
+ * pall - Prints all the values on the stack,
+ * starting from the top of the stack.
+ * @stack: The head of the stack
+ * @line_number: The line on which the error occurred
+ *
+ * Return: Nothing
  */
-void op_pall(stack_t **sp)
+void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *p = NULL;
+	stack_t *current = NULL;
+	(void) line_number;
 
-	if (*sp)
+	if (*stack)
 	{
-		(*sp)->next->prev = NULL;
-
-		for (p = *sp; p; p = p->prev)
-			printf("%d\n", p->n);
-
-		(*sp)->next->prev = *sp;
+		current = *stack;
+		while (current != NULL)
+		{
+			printf("%d\n", current->n);
+			current = current->next;
+		}
 	}
 }
--- op_pop.c
+++ op_pop.c
@@ -1,22 +1,64 @@
 #include "monty.h"
 
 /**
- * op_pop - remove the element at the top of the stack
- * @sp: stack pointer
- */
-void op_pop(stack_t **sp)
+  * push - Adds a new node at the beginning of the stack
+  * @stack: The head of the stack
+  * @param: The value to adds on the stack
+  *
+  * Return: Nothing
+  */
+void push(stack_t **stack, unsigned int param)
 {
-	stack_t *top = NULL;
+	stack_t *new_node = NULL;
 
-	if (!*sp)
-		pfailure("L%u: can't pop an empty stack\n", op_env.lineno);
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		handle_error(ERR_BAD_MALL, NULL, 0, NULL);
 
-	if (*sp != (*sp)->prev)
+	new_node->n = param;
+	if (*stack)
 	{
-		top = (*sp)->prev;
-		(*sp)->next->prev = top;
-		top->next = (*sp)->next;
+		new_node->next = *stack;
+		new_node->prev = (*stack)->prev;
+		(*stack)->prev = new_node;
+		*stack = new_node;
+		return;
 	}
-	free(*sp);
-	*sp = top;
+
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	*stack = new_node;
+}
+
+/**
+  * push_queue - Adds a new node at the end of the stack
+  * @stack: The head of the stack
+  * @param: The value to adds on the stack
+  *
+  * Return: Nothing
+  */
+void push_queue(stack_t **stack, unsigned int param)
+{
+	stack_t *current = NULL, *new_node = NULL;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		handle_error(ERR_BAD_MALL, NULL, 0, NULL);
+
+	new_node->n = param;
+	if (*stack)
+	{
+		current = *stack;
+		while (current->next != NULL)
+			current = current->next;
+
+		new_node->next = NULL;
+		new_node->prev = current;
+		current->next = new_node;
+		return;
+	}
+
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	*stack = new_node;
 }
