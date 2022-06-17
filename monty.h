6e4f7d1910ed6b3349a185fb576e6c22500753d5
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-17 02:02:08 +0100

    Updated header file

--- monty.h
+++ monty.h
@@ -1,8 +1,39 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef _MONTY_
+#define _MONTY_
 
-#include <stdio.h>
+/* Constants */
+#define SUCSS_OP		0
+#define VALID_PARM		0
+#define MIN_ARGS		2
+#define METH_STACK		300
+#define METH_QUEUE		301
+
+/* Common Errors */
+#define ERR_BAD_INST	100
+#define ERR_BAD_MALL	101
+#define ERR_INVLD_PARM	102
+
+/* Usage Errors */
+#define ERR_ARG_USG		200
+#define ERR_PUSH_USG	201
+#define ERR_PINT_USG	202
+#define ERR_POP_USG		203
+#define ERR_SWAP_USG	204
+#define ERR_ADD_USG		205
+#define ERR_SUB_USG		206
+#define ERR_DIV_USG		207
+#define ERR_DIV_ZRO		208
+#define ERR_MUL_USG		209
+#define ERR_MOD_USG		210
+#define ERR_PCH_USG		211
+#define ERR_PCH_EMP		212
+
+#include <ctype.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -20,12 +51,10 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
-typedef void (*instruction_fn)(stack_t **);
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
- * @fn: function to handle the opcode
+ * @f: function to handle the opcode
  *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
@@ -33,70 +62,39 @@ typedef void (*instruction_fn)(stack_t **);
 typedef struct instruction_s
 {
 	char *opcode;
-	instruction_fn fn;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/**
- * enum stack_mode_n - stack mode enumeration
- * @LIFO: operate as a stack
- * @FIFO: operate as a queue
- */
-typedef enum stack_mode_n
-{
-	LIFO = 0,
-	FIFO = 1
-} stack_mode_t;
+extern stack_t *head;
 
-/**
- * struct op_env_s - operation environment
- * @sp: top of the stack
- * @argv: argument vector
- * @line: line buffer
- * @linesz: line buffer size
- * @lineno: line number
- * @mode: stack operation mode
- */
-typedef struct op_env_s
-{
-	stack_t *sp;
-	char **argv;
-	char *line;
-	size_t linesz;
-	size_t lineno;
-	stack_mode_t mode;
-} op_env_t;
+void check_args_num(int argn);
+FILE *open_file(char *filename);
+void check_access_rights(char *filename);
+int check_push_param(char *param);
+int check_digits(char *s);
+void frees_stack(void);
+int handle_execution(char *op_code, char *op_param, unsigned int line, int m);
+void handle_error(int errno, char *opcode, unsigned int line, char *buff);
+void handle_cerror(int errno, char *opcode, unsigned int line);
+void handle_uerror(int errno, unsigned int line);
+void handle_more_uerror(int errno, unsigned int line);
+void (*pick_func(char *s))(stack_t **, unsigned int);
+unsigned int count_stack(stack_t *stack);
+void push(stack_t **stack, unsigned int param);
+void push_queue(stack_t **stack, unsigned int param);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
 
-extern op_env_t op_env;
-
-instruction_fn get_instruction_fn(const char *opcode);
-
-void op_add(stack_t **sp);
-void op_div(stack_t **sp);
-void op_mod(stack_t **sp);
-void op_mul(stack_t **sp);
-void op_nop(stack_t **sp);
-void op_pall(stack_t **sp);
-void op_pchar(stack_t **sp);
-void op_pint(stack_t **sp);
-void op_pop(stack_t **sp);
-void op_pstr(stack_t **sp);
-void op_push(stack_t **sp);
-void op_queue(stack_t **sp);
-void op_rotl(stack_t **sp);
-void op_rotr(stack_t **sp);
-void op_stack(stack_t **sp);
-void op_sub(stack_t **sp);
-void op_swap(stack_t **sp);
-
-char **tokenize(char *str);
-size_t count_tokens(const char *str);
-
-void free_op_env(void);
-void free_stack(stack_t **sp);
-
-void pfailure(const char *fmt, ...);
-
-int isinteger(const char *str);
-
-#endif /* MONTY_H */
-© 2022 GitHub, Inc.
+#endif
